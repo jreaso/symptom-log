9b9a82c3b671ddb6d20bb1bd1eeeddc246c75713
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-22 17:40:28 +0100

    added admin interface for Patient and Clinician models

--- authuser/admin.py
+++ authuser/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin import ModelAdmin
 
-from authuser.models import Account
+from authuser.models import Account, Patient, Clinician
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'username', 'first_name', 'last_name', 'role', 'date_joined', 'last_login', 'is_staff')
+    list_display = ('email', 'username', 'first_name', 'last_name', 'role', 'date_joined', 'last_login', 'is_staff', 'role_info')
     search_fields = ('email', 'username', 'first_name', 'last_name')
     readonly_fields = ('id', 'date_joined', 'last_login')
 
@@ -12,4 +13,33 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related('patient', 'clinician')
+
+    def role_info(self, obj):
+        if obj.role == 'patient' and hasattr(obj, 'patient'):
+            return f"DOB: {obj.patient.dob}, Sex: {obj.patient.sex}"
+        elif obj.role == 'clinician' and hasattr(obj, 'clinician'):
+            return f"Patients: {', '.join([str(patient) for patient in obj.clinician.patients.all()])}"
+        return "Admin - No Info"
+
+    role_info.short_description = "Role Info"
+
+class PatientAdmin(ModelAdmin):
+    list_display = ('user', 'dob', 'sex')
+    search_fields = ()
+    filter_horizontal = ()
+
+class ClinicianAdmin(ModelAdmin):
+    list_display = ('user', 'list_of_patients')
+    search_fields = ()
+
+    def list_of_patients(self, obj):
+        return ", ".join([str(patient) for patient in obj.patients.all()])
+
+    list_of_patients.short_description = "Patients"
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Clinician, ClinicianAdmin)
