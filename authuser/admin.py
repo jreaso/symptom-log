9de9871d9b2b1a654514ba658ca38d6b9a5dea1d
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-22 21:33:28 +0100

    reformatted with black

--- authuser/admin.py
+++ authuser/admin.py
@@ -4,54 +4,80 @@ from django.contrib.auth.admin import UserAdmin
 
 from authuser.models import Account, Patient, Clinician
 
+
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'role', 'date_joined', 'last_login', 'is_staff', 'role_info')
-    search_fields = ('email', 'first_name', 'last_name')
-    readonly_fields = ('id', 'date_joined', 'last_login')
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "date_joined",
+        "last_login",
+        "is_staff",
+        "role_info",
+    )
+    search_fields = ("email", "first_name", "last_name")
+    readonly_fields = ("id", "date_joined", "last_login")
 
     filter_horizontal = ()
     list_filter = ()
     ordering = ()
 
     fieldsets = (
-        (None, {'fields': ('first_name', 'last_name', 'email', 'role', 'password')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        ('Dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("first_name", "last_name", "email", "role", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Dates", {"fields": ("last_login", "date_joined")}),
     )
 
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'role', 'is_active', 'is_staff', 'is_superuser'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "role",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
     )
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
-        return queryset.select_related('patient', 'clinician')
+        return queryset.select_related("patient", "clinician")
 
     def role_info(self, obj):
-        if obj.role == 'patient' and hasattr(obj, 'patient'):
+        if obj.role == "patient" and hasattr(obj, "patient"):
             return f"DOB: {obj.patient.dob}, Sex: {obj.patient.sex}"
-        elif obj.role == 'clinician' and hasattr(obj, 'clinician'):
+        elif obj.role == "clinician" and hasattr(obj, "clinician"):
             return f"Patients: {', '.join([str(patient) for patient in obj.clinician.patients.all()])}"
         return "No Info"
 
     role_info.short_description = "Role Info"
 
+
 class PatientAdmin(ModelAdmin):
-    list_display = ('user', 'dob', 'sex')
+    list_display = ("user", "dob", "sex")
     search_fields = ()
     filter_horizontal = ()
 
+
 class ClinicianAdmin(ModelAdmin):
-    list_display = ('user', 'list_of_patients')
+    list_display = ("user", "list_of_patients")
     search_fields = ()
 
     def list_of_patients(self, obj):
         return ", ".join([str(patient) for patient in obj.patients.all()])
 
     list_of_patients.short_description = "Patients"
+
 
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Patient, PatientAdmin)
--- authuser/models.py
+++ authuser/models.py
@@ -3,46 +3,42 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class AccountManager(BaseUserManager):
-    def create_user(self, email, password=None, role='patient', **extra_fields):
+    def create_user(self, email, password=None, role="patient", **extra_fields):
         if not email:
             raise ValueError("Users must have an email address.")
-        
-        if role == 'admin':
+
+        if role == "admin":
             extra_fields.setdefault("is_staff", True)
-        
-        user = self.model(
-            email = self.normalize_email(email),
-            **extra_fields
-        )
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        
+
         return user
-    
+
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault("role", 'admin')
+        extra_fields.setdefault("role", "admin")
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get("role") != 'admin':
+        if extra_fields.get("role") != "admin":
             raise ValueError("Superuser must be admin users.")
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        user = self.model(
-            email = self.normalize_email(email),
-            **extra_fields
-        )
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+
 class Account(AbstractBaseUser):
     email = models.EmailField(verbose_name="email", max_length=60, unique=True)
-    #username = models.CharField(verbose_name="username", max_length=30, unique=True)
+    # username = models.CharField(verbose_name="username", max_length=30, unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
@@ -50,7 +46,9 @@ class Account(AbstractBaseUser):
     last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
 
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)  # Necessary when using AbstractBaseUser but not used
+    is_staff = models.BooleanField(
+        default=False
+    )  # Necessary when using AbstractBaseUser but not used
     is_superuser = models.BooleanField(default=False)
 
     class Role(models.TextChoices):
@@ -69,34 +67,39 @@ class Account(AbstractBaseUser):
         return f"{self.first_name} {self.last_name}"
 
     def has_perm(self, perm, obj=None):
-        return self.role == 'admin'
-    
+        return self.role == "admin"
+
     def has_module_perms(self, app_label):
         return True
- 
+
 
 class Patient(models.Model):
     user = models.OneToOneField(Account, on_delete=models.CASCADE, primary_key=True)
-    #patient_id = models.IntegerField(null=True, blank=True)
+    # patient_id = models.IntegerField(null=True, blank=True)
     dob = models.DateField(null=True, blank=True)
-    sex = models.CharField(max_length=10, choices=(('male', 'Male'), ('female', 'Female'), ('other', 'Other')), null=True, blank=True)
+    sex = models.CharField(
+        max_length=10,
+        choices=(("male", "Male"), ("female", "Female"), ("other", "Other")),
+        null=True,
+        blank=True,
+    )
 
 
 class Clinician(models.Model):
     user = models.OneToOneField(Account, on_delete=models.CASCADE)
-    patients = models.ManyToManyField(Patient, related_name='clinicians', blank=True)
+    patients = models.ManyToManyField(Patient, related_name="clinicians", blank=True)
 
 
 @receiver(post_save, sender=Account)
 def create_user_role_model(sender, instance, created, **kwargs):
     if created:
-        if instance.role.lower() == 'patient':
+        if instance.role.lower() == "patient":
             # Check if dob and sex exist in instance, otherwise use None
-            dob = getattr(instance, 'dob', None)
-            sex = getattr(instance, 'sex', None)
+            dob = getattr(instance, "dob", None)
+            sex = getattr(instance, "sex", None)
             Patient.objects.create(user=instance, dob=dob, sex=sex)
-        elif instance.role.lower() == 'clinician':
-            patients = getattr(instance, 'patients', None)
+        elif instance.role.lower() == "clinician":
+            patients = getattr(instance, "patients", None)
             clinician = Clinician.objects.create(user=instance)
             if patients is not None:
                 clinician.patients.set(patients)
