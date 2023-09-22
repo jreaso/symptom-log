0dd269c764cfcf35e12fdf83fbac196205812a3b
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-22 17:04:01 +0100

    added admin interface for Account model

--- authuser/admin.py
+++ authuser/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from authuser.models import Account
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'role', 'date_joined', 'last_login', 'is_staff')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    readonly_fields = ('id', 'date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
