38be03d630f5ce4f89f857d0001697567ad6725f
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-22 14:53:24 +0100

    changed user app to authuser

--- authuser/apps.py
+++ authuser/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserConfig(AppConfig):
+class AuthuserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user'
+    name = 'authuser'
