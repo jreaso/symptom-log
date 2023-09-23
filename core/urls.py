656d3e40b9e4744f030df578f52bf0693c524929
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-23 21:12:16 +0100

    added urls.py for patients app

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("authuser.urls")),  # Check this line
+    path("", include("authuser.urls")),
+    path("patients/", include("patients.urls")),
 ]
---
+++ patients/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    #path("", views.patients_list_view, name="patients_list"),
+]
