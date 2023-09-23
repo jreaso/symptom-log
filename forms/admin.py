f310e7459bdf88b0922892631752e02f1ee2cadc
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-23 12:52:24 +0100

    registering forms models with admin panel

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Form, Question, MultipleChoiceQuestion, StatusQuestion, EventQuestion, FormResponse
 
-# Register your models here.
+# Register models with the admin interface
+admin.site.register(Form)
+admin.site.register(Question)
+admin.site.register(MultipleChoiceQuestion)
+admin.site.register(StatusQuestion)
+admin.site.register(EventQuestion)
+admin.site.register(FormResponse)
