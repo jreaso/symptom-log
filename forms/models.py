c744a945df2ef848f9607a0ba9cf29ba4981a4e4
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-23 19:55:54 +0100

    fix form models

--- forms/models.py
+++ forms/models.py
@@ -5,73 +5,10 @@ from django.contrib.contenttypes.fields import GenericRelation
 from authuser.models import Account
 
 
-class Question(models.Model):
-    question_title = models.CharField(max_length=50)
-    label = models.CharField(max_length=30, null=True, blank=True)  # How this question should be labelled on graphs etc
-
-    def __str__(self):
-        if self.label:
-            return self.label
-        return self.question_title
-
-
-class SymptomScoreQuestion(Question):
-    pass
-
-class SymptomScoreResponse(models.Model):
-    score = models.PositiveIntegerField()
-
-    def __str__(self):
-        return str(self.score)
-
-
-class TextQuestion(Question):
-    pass
-
-class TextResponse(models.Model):
-    text = models.TextField()
-
-    def __str__(self):
-        return self.text
-
-
-class MultipleChoiceQuestion(Question):
-    options = models.CharField(max_length=255, null=True, blank=True)  # Store options as a comma-separated text
-
-class MultipleChoiceResponse(models.Model):
-    choice = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.choice
-
-
-class StatusQuestion(Question):
-    option_0 = models.CharField(max_length=10, null=True, blank=True)  # Label for False, Off, 0 etc
-    option_1 = models.CharField(max_length=10, null=True, blank=True)  # Label for True, On, 1 etc
-
-class StatusResponse(models.Model):
-    status = models.BooleanField()
-
-    def __str__(self):
-        return str(self.status)
-
-
-class EventQuestion(Question):
-    pass
-
-class EventResponse(models.Model):
-    event_datetime = models.DateTimeField()
-
-    def __str__(self):
-        return str(self.event_datetime)
-
-
 class Form(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
     patient = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='forms')
-
-    questions = models.ManyToManyField(Question, related_name='forms')
 
     date_created = models.DateTimeField(auto_now_add=True)
     last_edited = models.DateTimeField(auto_now=True)
@@ -80,17 +17,62 @@ class Form(models.Model):
         return f"{self.patient} - {self.title}"
 
 
-class FormResponse(models.Model):
-    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='responses')
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='responses')
-    
-    question_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    question_object_id = models.PositiveIntegerField()
-    question = GenericForeignKey('question_content_type', 'question_object_id')
+class Question(models.Model):
+    question_title = models.CharField(max_length=50)
+    label = models.CharField(max_length=30, null=True, blank=True)  # How this question should be labelled on graphs etc
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='questions')
 
+    def __str__(self):
+        if self.label:
+            return self.label
+        return self.question_title
+
+
+class Response(models.Model):
     submitted_at = models.DateTimeField(auto_now_add=True)
     submitted_by = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='submitted_responses')
 
     def __str__(self):
-        return f"{self.form} ({self.date_submitted})"
+        return f"{self.question} ({self.date_submitted})"
+
+
+class SymptomScoreQuestion(Question):
+    pass
+
+class SymptomScoreResponse(Response):
+    question = models.ForeignKey(SymptomScoreQuestion, on_delete=models.CASCADE, related_name='responses')
+    score = models.PositiveIntegerField()
+
+
+class TextQuestion(Question):
+    pass
+
+class TextResponse(Response):
+    question = models.ForeignKey(TextQuestion, on_delete=models.CASCADE, related_name='responses')
+    text = models.TextField()
+
+
+class MultipleChoiceQuestion(Question):
+    options = models.CharField(max_length=255, null=True, blank=True)  # Store options as a comma-separated text
+
+class MultipleChoiceResponse(Response):
+    question = models.ForeignKey(MultipleChoiceQuestion, on_delete=models.CASCADE, related_name='responses')
+    choice = models.CharField(max_length=255)
+
+
+class StatusQuestion(Question):
+    option_0 = models.CharField(max_length=10, null=True, blank=True)  # Label for False, Off, 0 etc
+    option_1 = models.CharField(max_length=10, null=True, blank=True)  # Label for True, On, 1 etc
+
+class StatusResponse(Response):
+    question = models.ForeignKey(StatusQuestion, on_delete=models.CASCADE, related_name='responses')
+    status = models.BooleanField()
+
+
+class EventQuestion(Question):
+    pass
+
+class EventResponse(Responsel):
+    question = models.ForeignKey(EventQuestion, on_delete=models.CASCADE, related_name='responses')
+    event_datetime = models.DateTimeField()
 
