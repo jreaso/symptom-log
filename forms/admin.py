a7339cc93167bc6247aa1ce99f56bc93d3a35a6c
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-23 19:03:37 +0100

    registered updated models with admin

--- forms/admin.py
+++ forms/admin.py
@@ -1,14 +1,66 @@
 from django.contrib import admin
-from .models import Form, Question, MultipleChoiceQuestion, StatusQuestion, EventQuestion, FormResponse
-from .forms import FormResponseForm
+from .models import (
+    SymptomScoreQuestion,
+    SymptomScoreResponse,
+    TextQuestion,
+    TextResponse,
+    MultipleChoiceQuestion,
+    MultipleChoiceResponse,
+    StatusQuestion,
+    StatusResponse,
+    EventQuestion,
+    EventResponse,
+    Form,
+    FormResponse,
+)
+
+# Define admin classes for each model
+@admin.register(SymptomScoreQuestion)
+class SymptomScoreQuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_title', 'label')
+
+@admin.register(SymptomScoreResponse)
+class SymptomScoreResponseAdmin(admin.ModelAdmin):
+    list_display = ('score',)
+
+@admin.register(TextQuestion)
+class TextQuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_title', 'label')
+
+@admin.register(TextResponse)
+class TextResponseAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+@admin.register(MultipleChoiceQuestion)
+class MultipleChoiceQuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_title', 'label')
+
+@admin.register(MultipleChoiceResponse)
+class MultipleChoiceResponseAdmin(admin.ModelAdmin):
+    list_display = ('choice',)
+
+@admin.register(StatusQuestion)
+class StatusQuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_title', 'label')
+
+@admin.register(StatusResponse)
+class StatusResponseAdmin(admin.ModelAdmin):
+    list_display = ('status',)
+
+@admin.register(EventQuestion)
+class EventQuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_title', 'label')
+
+@admin.register(EventResponse)
+class EventResponseAdmin(admin.ModelAdmin):
+    list_display = ('event_datetime',)
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('title', 'patient', 'date_created', 'last_edited')
+    filter_horizontal = ('questions',)  # Use a filter_horizontal widget for the questions field
 
 @admin.register(FormResponse)
 class FormResponseAdmin(admin.ModelAdmin):
-    form = FormResponseForm
+    list_display = ('form', 'question', 'submitted_at', 'submitted_by')
 
-# Register other models with the admin interface
-admin.site.register(Form)
-admin.site.register(Question)
-admin.site.register(MultipleChoiceQuestion)
-admin.site.register(StatusQuestion)
-admin.site.register(EventQuestion)
