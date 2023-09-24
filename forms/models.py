cf117d741e40912258a5fa01bf77995dcf9a2d2f
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-24 20:54:57 +0100

    added order to questions

--- forms/models.py
+++ forms/models.py
@@ -20,6 +20,18 @@ class Question(models.Model):
     label = models.CharField(max_length=30, null=True, blank=True)  # How this question should be labelled on graphs etc
     form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='questions')
 
+    order = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
+    
+    def save(self, *args, **kwargs):
+        # Automatically assign the order when saving a new question
+        if not self.order:
+            last_question = Question.objects.filter(form=self.form).order_by('-order').first()
+            if last_question:
+                self.order = last_question.order + 1
+            else:
+                self.order = 1
+        super(Question, self).save(*args, **kwargs)
+
     def __str__(self):
         if self.label:
             return self.label
