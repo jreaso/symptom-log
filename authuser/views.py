e23d384bbeef9f254733b1566ff47fb65b74f8de
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-24 12:42:53 +0100

    updated login page to handle more errors

--- authuser/views.py
+++ authuser/views.py
@@ -2,19 +2,41 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
+from .models import Account
 
 
 def login_view(request):
     if request.method == "POST":
         # Accessing username and password from form data
-        username = request.POST["username"]
+        email = request.POST["email"]
         password = request.POST["password"]
 
+        if not email:
+            return render(
+                request,
+                "authuser/login.html",
+                {"title": "Login", "message": "Please Enter Email"},
+            )
+
+        if not password:
+            return render(
+                request,
+                "authuser/login.html",
+                {"title": "Login", "message": "Please Enter Password"},
+            )
+        
+        if not Account.objects.filter(email=email).exists():
+            return render(
+                request,
+                "authuser/login.html",
+                {"title": "Login", "message": "Account Does Not Exist", "email": email},
+            )
+
         # Check if username and password are correct, returning User object if so
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
 
         # If user object is returned, log in and route to index page:
-        if user:
+        if user is not None:
             login(request, user)
             # return HttpResponseRedirect(reverse("dashboard"))
             return HttpResponse("Logged In")
@@ -23,7 +45,7 @@ def login_view(request):
             return render(
                 request,
                 "authuser/login.html",
-                {"title": "Login", "message": "Invalid Credentials"},
+                {"title": "Login", "message": "Invalid Credentials", "email": email},
             )
     else:
         # Check if user is already logged in
