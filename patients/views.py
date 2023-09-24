529a693b310064bc445a6498870ec84375039fc8
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-24 11:44:12 +0100

    updated patients_list view

--- patients/views.py
+++ patients/views.py
@@ -2,19 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.core.exceptions import PermissionDenied
-#from core.models import Patient, Account
+from authuser.models import Patient, Account
 
 
 @login_required
 def patients_list(request):
-    if request.user.role in ["admin", "clinician"]:
-        if request.user.role == "admin":
-            #patients = Patient.objects.all()
-            patients = None
-        else:
-            # For clinicians, display only their related patients
+    if request.user.role in [Account.Role.ADMIN, Account.Role.CLINICIAN]:
+        if request.user.role == Account.Role.ADMIN:
+            patients = Patient.objects.all()
+        elif request.user.role == Account.Role.CLINICIAN:
             patients = request.user.clinician.patients.all()
-        
-        return render(request, "patients/patient_list.html", {"patients": patients})
+        return render(request, "patients/patients_list.html", {"patients": patients})
     else:
         raise PermissionDenied
