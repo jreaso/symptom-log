feefae54b1082608f338478294c5fa97d2a6b37d
Jamie Reason <95385925+jamiereason@users.noreply.github.com>
2023-09-24 16:43:44 +0100

    updated forms models

--- forms/models.py
+++ forms/models.py
@@ -24,21 +24,29 @@ class Question(models.Model):
         if self.label:
             return self.label
         return self.question_title
+    
+
+class FormResponse(models.Model):
+    form = models.ForeignKey(Form, on_delete=models.CASCADE, related_name='responses')
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    submitted_by = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='form_responses')
+
+    def __str__(self):
+        return f"{self.form} ({self.submitted_at})"
 
 
 class Response(models.Model):
-    submitted_at = models.DateTimeField(auto_now_add=True)
-    submitted_by = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='submitted_responses')
+    form_response = models.ForeignKey(FormResponse, on_delete=models.CASCADE, null=True, related_name='questions')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='responses')
 
     def __str__(self):
-        return f"{self.question} ({self.submitted_at})"
+        return f"{self.question} ({self.form_response.submitted_at})"
 
 
 class SymptomScoreQuestion(Question):
     pass
 
 class SymptomScoreResponse(Response):
-    question = models.ForeignKey(SymptomScoreQuestion, on_delete=models.CASCADE, related_name='responses')
     score = models.PositiveIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(10)]
     )
@@ -48,7 +56,6 @@ class TextQuestion(Question):
     pass
 
 class TextResponse(Response):
-    question = models.ForeignKey(TextQuestion, on_delete=models.CASCADE, related_name='responses')
     text = models.TextField()
 
 
@@ -56,7 +63,6 @@ class MultipleChoiceQuestion(Question):
     options = models.CharField(max_length=255, null=True, blank=True)  # Store options as a comma-separated text
 
 class MultipleChoiceResponse(Response):
-    question = models.ForeignKey(MultipleChoiceQuestion, on_delete=models.CASCADE, related_name='responses')
     choice = models.CharField(max_length=255)
 
 
@@ -65,7 +71,6 @@ class StatusQuestion(Question):
     option_1 = models.CharField(max_length=10, null=True, blank=True)  # Label for True, On, 1 etc
 
 class StatusResponse(Response):
-    question = models.ForeignKey(StatusQuestion, on_delete=models.CASCADE, related_name='responses')
     status = models.BooleanField()
 
 
@@ -73,6 +78,5 @@ class EventQuestion(Question):
     pass
 
 class EventResponse(Response):
-    question = models.ForeignKey(EventQuestion, on_delete=models.CASCADE, related_name='responses')
     event_datetime = models.DateTimeField()
 
